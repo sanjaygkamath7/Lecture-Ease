8a35be013ad60957995964059897721cdbfe4746
Sanjay Kamath <sanjaykamath@canaraengineering.in>
2025-06-16 17:24:39 +0530

    backend update

--- backend/Server.js
+++ backend/Server.js
@@ -11,7 +11,12 @@ const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+const cors = require("cors");
+app.use(cors({
+  origin: "https://lecturease.tech",
+  credentials: true
+}));
 
 
 mongoose
@@ -26,14 +31,8 @@ mongoose
 app.use("/api", authRoutes);
 
 
-// app.use(cors())
-const cors = require("cors");
-app.use(cors({
-  origin: "https://lecturease.tech",
-  credentials: true
-}));
-
-app.use(express.json());
+// // app.use(cors()
+// app.use(express.json());
 
 
 // const uploadPath =
